dc50d68f486a2cd56c716091402e088c29729015
Ryan B Flynn <Ryan.b.flynn@gmail.com>
2025-05-10 22:40:50 -0400

    updating file_exporter.py

--- exporter/file_exporter.py
+++ exporter/file_exporter.py
@@ -1,6 +1,17 @@
+import csv
+from io import StringIO
+
 class FileExporter:
-    def export_to_csv(self, schedule, filename):
-        pass
+    def export_to_csv(self, schedule, filename=None):
+        output = StringIO()
+        writer = csv.DictWriter(output, fieldnames=["course", "block", "duration", "date"])
+        writer.writeheader()
+        writer.writerows(schedule)
+        return output.getvalue()
     
-    def export_to_txt(self, schedule, filename):
-        pass
+    def export_to_txt(self, schedule, filename=None):
+        lines = [
+            f"{entry['date']} | {entry['course']} | {entry['block']} | {entry['duration']} min"
+            for entry in schedule
+        ]
+        return "\n".join(lines)
